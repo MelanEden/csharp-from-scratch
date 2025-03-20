3f09b82532854dfb023df1c5355e18d8f9c35667
MelanE <edenmelan8701@.com>
2025-03-20 11:21:25 -0500

    actualizar carpetas

---
+++ final/Challenges/Challenge1.cs
@@ -0,0 +1,38 @@
+﻿namespace itm.csharp.basic
+{
+    public class Challenge1
+    {
+public void Run()
+        {
+            Console.WriteLine("Ingrese un número:");
+            int num = int.Parse(Console.ReadLine());
+            if (num > 0) Console.WriteLine("Positivo")
+            else if (num < 0) Console.WriteLine("Negativo");
+            else Console.WriteLine("Es cero");
+        }
+    }
+
+    public class Challenge2
+    {
+        public void Run()
+        {
+            Console.WriteLine("Ingrese primer número:" );
+            double num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese segundo número:" );
+            double num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Suma: {num1 + num2}\nResta: {num1 - num2}\nMultiplicación: {num1 * num2}\nDivisión:{num1 / num2} ");
+
+        }
+    }
+
+    public class Challenge3
+    {
+        public void Run()
+        {
+         Console.WriteLine("Ingrese un número:");
+         int numero = int.Parse(Console.ReadLine());
+          Console.WriteLine($"El cuadrado de {numero} es: {numero * numero}");
+        }
+    }
+
+    }//Cierra toda la clase
---
+++ final/Challenges/Program.cs
@@ -0,0 +1,38 @@
+namespace itm.csharp.basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su nombre:");
+            string nombre = Console.ReadLine();
+            Console.WriteLine($"¡Hola, {nombre}! Por favor, elige un ejercicio del siguiente menú:");
+            Menu.MostrarMenu();
+
+            while (true)
+            {
+                string entrada = Console.ReadLine().ToLower();
+
+                if (entrada == "q" || entrada == "exit")
+                {
+                    break; // Salir del bucle
+                }
+
+                if (int.TryParse(entrada, out int eleccion) && eleccion >= 1 && eleccion <= 15)
+                {
+                    // Aquí puedes agregar la lógica del switch para ejecutar los ejercicios...
+
+                    Console.WriteLine("Presiona una tecla para continuar...");
+                    Console.ReadKey();
+                    Console.Clear();
+                    Menu.MostrarMenu();
+                }
+                else
+                {
+                    Console.WriteLine("Opción no válida. Por favor, ingrese un número entre 1 y 15, o 'q' o 'exit' para salir.");
+                }
+            }
+
+            Console.WriteLine("¡Hasta luego!");
+        }
+    }
